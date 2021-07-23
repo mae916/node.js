d2a0d1e5c4f6b7a5a7a3c9d691557e1da12fce46
mae916 <ruchia916@naver.com>
2021-07-23 17:12:26 +0900

    node.js. 학습

---
+++ exam/file/file_access.js
@@ -0,0 +1,36 @@
+const fs = require('fs').promises;
+const constants = require('fs').constants; // F_OK, W_OK, R_OK
+/*
+fs.access('./logs',constants.F_OK)
+  .then(() => {
+    console.log("logs 폴더 존재");
+  })
+  .catch((err) => {
+    //err.code ---'ENOENT' -> 폴더나 파일이 없다
+    //console.log(err.code);
+    //console.log("logs 폴더 없음");
+    if(err.code=='ENOENT') {
+      return fs.mkdir("./logs");
+    }
+  });
+  .then(() => {
+    console.log("logs 폴더 생성");
+  })
+  .catch((err) => {
+    console.error(err);
+  })
+*/
+
+(async() => {
+  try {
+  await fs.access("./logs",constants.F_OK) // 폴더가 존재하면, 존재X -> catch
+  console.log("폴더 있음");
+} catch (err){
+  console.log(err);
+  //폴더, 파일 접속권한 X
+  if (err.code=='ENOENT') {
+    console.log("폴더 없음");
+    await fs.mkdir("./logs")
+    }
+}
+})();
